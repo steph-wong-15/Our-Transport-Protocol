2e3f767a999fa92ac8a3adcf0c4082a10ca8ab0a
SharanjitV101 <sharanjitkaurvirdi@gmail.com>
2023-12-04 10:40:45 -0800

    updatated handshake and close connection

--- ProtocolServer.py
+++ ProtocolServer.py
@@ -9,14 +9,16 @@ class ProtocolServer:
         self.server_socket.bind((host, port))
         self.server_socket.listen(1) # server begins listening to incoming requests
 
-
     def perform_handshake(self, client_socket):
         # Step 1: Receive SYN from client
         syn_packet = self.receive_data(client_socket)
         if syn_packet != "SYN":
             raise Exception("Handshake failed - SYN not received")
         
-        # Step 2: SYN_ACK from server --> add step before:  chnage it to send SYN, and then ACK
+        # Step 2: Send a SYN and ACK together from server
+        self.send_data(client_socket, "SYN-ACK")
+        
+        # Step 3: Receive ACK from the client
         ack_packet = self.receive_data(client_socket)
         if ack_packet != "ACK":
             raise Exception("Handshake failed - ACK not received")
@@ -32,15 +34,15 @@ class ProtocolServer:
                 break
 
         # Server sends FIN
-        self.send_data(client_socket, "FIN") #+ send ACK
+        self.send_data(client_socket, "FIN-ACK")
 
         # Receive ACK from client (add timeout)
         ack_packet = self.receive_data(client_socket, timeout=5)
         if ack_packet != "ACK":
-            raise Exception("Failed to received ACK after FIN")
+            raise Exception("Failed to received ACK after FIN-ACK")
         
-        # Close connction
-        self.close_connection(client_socket, client_address)
+        # Close connection
+        self.server_socket.close()
 
     def receive_data(self, client_socket, bufsize=1024, timeout=None):
         data = client_socket.recv(bufsize).decode('utf-8')
@@ -52,13 +54,6 @@ class ProtocolServer:
         packet = header + message
 
         client_socket.sendall(packet)
-
-    def close_connection(self, client_socket, client_address): # probably just .close() since FIN+ACK is done
-        # Server get FIN packet from client
-        fin_packet = self.receive_data(client_socket, timeout=5)
-        if fin_packet == "FIN":
-            self.send_data(client_socket, "ACK")
-            # NEED TO close client socket in client side
 
     def start(self):
         print("Server listening")
